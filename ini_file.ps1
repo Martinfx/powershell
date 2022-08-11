a43cf0176d814e2209276592815bbd292f3ec4f1
M <1999714+Martinfx@users.noreply.github.com>
2022-08-11 19:35:34 +0200

    Update ini_file.ps1

--- ini_file.ps1
+++ ini_file.ps1
@@ -1,21 +1,28 @@
 
+
 if (Get-Module -ListAvailable -Name IniManager) {
     Write-Host "Module exists"
 } 
 else {
     Write-Host "Module does not exist. I will install .."
     Install-Module -Name IniManager
-  
+
 }
-    $obj = Get-Ini -Path "C:\test\test.ini"
-    write $obj.{[owner]}.'name'
-    write $obj.{[owner]}.'organization'
+
+
+if(Test-Path -Path "C:\test\test.ini" -Verbose ) {
+$obj = Get-Ini -Path C:\test\test.ini
+write $obj.{[owner]}.'name '
+write $obj.{[owner]}.'organization '
+} else {
+    write "File not exist! C:\test\test.ini"
+}
     
-    if(Test-Path -Path C:\test.ini) {
-       write "file exist!"
-    } 
-    else{
-        New-Item -Path 'C:\test.ini' -ItemType File
-    }
+if(Test-Path -Path C:\test.ini) {
+   write "file exist!"
+} 
+else {
+   New-Item -Path 'C:\test.ini' -ItemType File
+}
     
 
